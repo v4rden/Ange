01ac125d7a2924c223ed29eb767da924122f9924
v4rden <denys.variah@gmail.com>
2019-07-15 23:52:46 +0300

    Added db sets.

--- Ange.Application/Interfaces/IAngeDbContext.cs
+++ Ange.Application/Interfaces/IAngeDbContext.cs
@@ -2,9 +2,15 @@ namespace Ange.Application.Interfaces
 {
     using System.Threading;
     using System.Threading.Tasks;
+    using Domain.Entities;
+    using Microsoft.EntityFrameworkCore;
 
     public interface IAngeDbContext
     {
+        DbSet<User> Users { get; set; }
+        DbSet<Room> Rooms { get; set; }
+        DbSet<ChatMessage> ChatMessages { get; set; }
+
         Task<int> SaveChangesAsync(CancellationToken cancellationToken);
     }
 }

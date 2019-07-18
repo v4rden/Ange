e34e9f2e5fa58845cfb514f2bc8de5a620257bea
v4rden <denys.variah@gmail.com>
2019-07-18 21:25:23 +0300

    Fromated.

--- Ange.Application/Exceptions/NotFoundException.cs
+++ Ange.Application/Exceptions/NotFoundException.cs
@@ -2,7 +2,7 @@ namespace Ange.Application.Exceptions
 {
     using System;
 
-    public class NotFoundException :Exception
+    public class NotFoundException : Exception
     {
         public NotFoundException(string name, object key)
             : base($"Entity \"{name}\" ({key}) was not found.")

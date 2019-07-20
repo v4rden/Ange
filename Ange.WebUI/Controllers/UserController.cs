d9e8cce966e78b0ac089d888ad3dd4e6cc1d8ffa
v4rden <denys.variah@gmail.com>
2019-07-20 19:30:41 +0300

    Added simple user controller.

---
+++ Ange.WebUI/Controllers/UserController.cs
@@ -0,0 +1,60 @@
+namespace Ange.WebUI.Controllers
+{
+    using System;
+    using System.Threading.Tasks;
+    using Application.User.Commands.CreateUser;
+    using Application.User.Commands.DeleteUser;
+    using Application.User.Commands.UpdateUser;
+    using Application.User.Queries.GetUserDetail;
+    using Application.User.Queries.GetUserList;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class UserController : BaseController
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<UserListViewModel>> GetAll()
+        {
+            return Ok(await Mediator.Send(new GetUserListQuery()));
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<UserDetailModel>> Get(Guid id)
+        {
+            return Ok(await Mediator.Send(new GetUserDetailQuery {Id = id}));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> Create([FromBody] CreateUserCommand command)
+        {
+            await Mediator.Send(command);
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Update([FromBody] UpdateUserCommand command)
+        {
+            await Mediator.Send(command);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await Mediator.Send(new DeleteUserCommand {Id = id});
+
+            return NoContent();
+        }
+    }
+}

648345fcc3fd6e2631930eadd5b96d0d3cb31a8f
v4rden <denys.variah@gmail.com>
2019-07-20 22:03:28 +0300

    Apply migrations and use initializer.

--- Ange.WebUI/Program.cs
+++ Ange.WebUI/Program.cs
@@ -1,14 +1,38 @@
 namespace Ange.WebUI
 {
+    using System;
+    using Application.Interfaces;
     using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Logging;
+    using Persistence;
 
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                try
+                {
+                    var context = scope.ServiceProvider.GetService<IAngeDbContext>();
+
+                    var concreteContext = (AngeDbContext) context;
+                    concreteContext.Database.Migrate();
+                    AngeDbInitializer.Initialize(concreteContext);
+                }
+                catch (Exception ex)
+                {
+                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while migrating or initializing the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>

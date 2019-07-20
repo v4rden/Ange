2e083e38add72b68648cfce391f30577a3ed60dd
v4rden <denys.variah@gmail.com>
2019-07-20 21:10:23 +0300

    Added configuration of relations.

--- Ange.Persistence/Configurations/ChatMessageConfiguration.cs
+++ Ange.Persistence/Configurations/ChatMessageConfiguration.cs
@@ -9,12 +9,20 @@ namespace Ange.Persistence.Configurations
         public void Configure(EntityTypeBuilder<ChatMessage> builder)
         {
             builder.Property(x => x.Id).IsRequired().ValueGeneratedNever();
-            builder.Property(x => x.Room).IsRequired();
-            builder.Property(x => x.Author).IsRequired();
+            builder.Property(x => x.RoomId).IsRequired();
+            builder.Property(x => x.AuthorId).IsRequired();
             builder.Property(x => x.SentTime).IsRequired();
-            builder.Property(x => x.LastModified).IsRequired();
+            builder.Property(x => x.LastModified);
             builder.Property(x => x.Type).IsRequired();
             builder.Property(x => x.MessageText).IsRequired();
+
+            builder.HasOne(c => c.Author)
+                .WithMany(u => u.Messages)
+                .HasForeignKey(c => c.AuthorId);
+
+            builder.HasOne(c => c.Room)
+                .WithMany(r => r.Messages)
+                .HasForeignKey(c => c.RoomId);
         }
     }
 }
---
+++ Ange.Persistence/Configurations/UserRoomConfiguration.cs
@@ -0,0 +1,23 @@
+namespace Ange.Persistence.Configurations
+{
+    using Domain.Entities;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public class UserRoomConfiguration : IEntityTypeConfiguration<UserRoom>
+    {
+        public void Configure(EntityTypeBuilder<UserRoom> builder)
+        {
+            builder.HasKey(ur => new {ur.User, ur.Room})
+                .ForSqlServerIsClustered(false);
+
+            builder.HasOne(ur => ur.User)
+                .WithMany(u => u.RecideRooms)
+                .HasForeignKey(ur => ur.UserId);
+
+            builder.HasOne(ur => ur.Room)
+                .WithMany(r => r.Participants)
+                .HasForeignKey(ur => ur.RoomId);
+        }
+    }
+}

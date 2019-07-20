b63eb12a86a8fbbe9097c17efdcfc19bebde8410
v4rden <denys.variah@gmail.com>
2019-07-20 21:36:57 +0300

    Finished implementing update command for chat message.

--- Ange.Application/ChatMessage/Commands/UpdateChatMessage/ChatMessageUpdateCommand.cs
+++ Ange.Application/ChatMessage/Commands/UpdateChatMessage/ChatMessageUpdateCommand.cs
@@ -1,6 +1,58 @@
 namespace Ange.Application.ChatMessage.Commands.UpdateChatMessage
 {
-    public class ChatMessageUpdateCommand
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Domain.Enumerations;
+    using Exceptions;
+    using Interfaces;
+    using MediatR;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ChatMessageUpdateCommand : IRequest
     {
+        public Guid Id { get; set; }
+        public Guid RoomId { get; set; }
+        public Guid AuthorId { get; set; }
+        public DateTime SentTime { get; set; }
+        public DateTime LastModified { get; set; }
+        public MessageType Type { get; set; }
+        public string MessageText { get; set; }
+    }
+
+    public class ChatMessageUpdateCommandHandler : IRequestHandler<ChatMessageUpdateCommand, Unit>
+    {
+        private readonly IAngeDbContext _context;
+        private readonly IMediator _mediator;
+
+        public ChatMessageUpdateCommandHandler(IAngeDbContext context, IMediator mediator)
+        {
+            _context = context;
+            _mediator = mediator;
+        }
+
+        public async Task<Unit> Handle(ChatMessageUpdateCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.ChatMessages
+                .SingleOrDefaultAsync(c => c.Id == request.Id, cancellationToken);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(User), request.Id);
+            }
+
+            entity.RoomId = request.RoomId;
+            entity.AuthorId = request.AuthorId;
+            entity.SentTime = request.SentTime;
+            entity.LastModified = request.LastModified;
+            entity.Type = request.Type;
+            entity.MessageText = request.MessageText;
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            await _mediator.Publish(new ChatMessageUpdated {Id = entity.Id}, cancellationToken);
+
+            return Unit.Value;
+        }
     }
 }
--- Ange.Application/ChatMessage/Commands/UpdateChatMessage/ChatMessageUpdateCommandValidator.cs
+++ Ange.Application/ChatMessage/Commands/UpdateChatMessage/ChatMessageUpdateCommandValidator.cs
@@ -1,6 +1,18 @@
 namespace Ange.Application.ChatMessage.Commands.UpdateChatMessage
 {
-    public class ChatMessageUpdateCommandValidator
+    using FluentValidation;
+
+    public class ChatMessageUpdateCommandValidator : AbstractValidator<ChatMessageUpdateCommand>
     {
+        public ChatMessageUpdateCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty();
+            RuleFor(x => x.RoomId).NotEmpty();
+            RuleFor(x => x.AuthorId).NotEmpty();
+            RuleFor(x => x.SentTime).NotEmpty();
+            RuleFor(x => x.LastModified).GreaterThan(x => x.SentTime).NotEmpty();
+            RuleFor(x => x.Type).NotEmpty();
+            RuleFor(x => x.MessageText).NotEmpty();
+        }
     }
 }

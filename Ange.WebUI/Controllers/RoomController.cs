c551d28d53fa65289dc7d04fc7df239f87a93c48
v4rden <denys.variah@gmail.com>
2019-07-26 17:52:18 +0300

    Added search by id.

--- Ange.WebUI/Controllers/RoomController.cs
+++ Ange.WebUI/Controllers/RoomController.cs
@@ -1,6 +1,8 @@
 namespace Ange.WebUI.Controllers
 {
+    using System;
     using System.Threading.Tasks;
+    using Application.Room.Queries.GetRoomDetail;
     using Application.Room.Queries.GetRoomsList;
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
@@ -12,6 +14,14 @@ namespace Ange.WebUI.Controllers
         public async Task<ActionResult<RoomListViewModel>> GetAll()
         {
             return Ok(await Mediator.Send(new GetRoomListQuery()));
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<RoomDetailModel>> Get(Guid id)
+        {
+            return Ok(await Mediator.Send(new GetRoomDetailQuery {Id = id}));
         }
     }
 }
